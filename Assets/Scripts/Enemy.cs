66bbee1614a1e57ba53f3663718ca6dcc098838a
Nicholas <nbgilden@fullsail.edu>
2018-04-21 12:19:10 -0500

    Added enemy and interfaces

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public bool Activated = false;
+    [SerializeField]
+    Transform SightTrans = null;
+    [SerializeField]
+    Transform[] gunTransforms = null;
+    [SerializeField]
+    IProjectile Bullet = null;
+    [SerializeField]
+    float ROF = 1.0f;
+    int currGunSlot = 0;
+    float fireTimeing = 0;
+    float currFireTime = 0;
+    GameObject Player = null;
+	// Use this for initialization
+	void Start ()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        fireTimeing = 1 / ROF;
+        currFireTime = fireTimeing;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        currFireTime += Time.deltaTime;
+        if (Activated && Player != null)
+        {
+            ShootAtPlayer();
+            //May or may not hide behind cover
+        }
+	}
+
+    void ShootAtPlayer()
+    {
+        //Look at player
+        transform.LookAt(Player.transform);
+        //Can we see player
+        if (CheckSight())
+        {
+            //Shoot at player
+            if(currFireTime >= fireTimeing)
+            {
+
+            }
+        }
+    }
+
+    bool CheckSight()
+    {
+        Ray ray = new Ray(SightTrans.position, SightTrans.forward);
+        RaycastHit hit;
+         if(Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.tag == "Player")
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/Interfaces.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IProjectile
+{
+    void Shoot(Vector3 direction);
+    void Shoot(Ray direction);
+    void Shoot(Ray direction, float speed);
+    void Shoot(Vector3 direction, float speed);
+}
+
+public interface IDamageable
+{
+    void Damage(float damageTaken);
+}
